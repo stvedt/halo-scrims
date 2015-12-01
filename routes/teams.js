f8e930a6b3ec39681742a9423f2774fceb401060
Stephen Tvedt <stvedt@kbsp.com>
2015-12-01 10:45:07 -0500

    set up get/set for teams

--- routes/teams.js
+++ routes/teams.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var dataEnvyUs = {
+  name: "EnVyUs",
+  players: ['Mikwen','APG','Heinz','Pistola']
+}
+
+
+var TeamSchema = require('../schemas/teams');
+
+var record = new TeamSchema(dataEnvyUs);
+
+
 router.get('/', function(req, res, next){
 
     res.render('default', {
@@ -9,12 +20,39 @@ router.get('/', function(req, res, next){
     });
 });
 
+router.use('/set',function(req, res, next){
+    record.save(function(err){
+      if(err){
+        console.log(err);
+        res.status(500).json({status:'failure'});
+      } else {
+        console.log('success');
+        res.json({status:'success'});
+      }
+
+    });
+});
+
+router.use('/get',  function(req, res, next){
+
+    TeamSchema.find().exec(function(err, teams){
+        if(err){
+            console.log(err);
+            res.status(500).json({status:'failure'});
+        } else {
+            console.log('success');
+            res.json(teams);
+          }
+    });
+});
+
 router.get('/:teamName?',  function(req, res, next){
     var name = req.params.name;
 
     res.render('default', {
         pageTitle: 'Team: ' + req.params.teamName,
-        className: 'team-single'
+        className: 'team-single',
+        players: 
     });
 });
 
